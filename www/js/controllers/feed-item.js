ced249e1701a536e79f548b489823ff72eaabc1e
Pasi Lampinen <pasi.lampinen@futurice.com>
2017-03-23 10:19:57 +0200

    refactor single smile fetching

--- www/js/controllers/feed-item.js
+++ www/js/controllers/feed-item.js
@@ -1,24 +1,26 @@
 angular.module('smileyApp.controllers')
 
 .controller('FeedItemCtrl', function($scope, $stateParams, SmileyService, Helpers) {
+
+  // Single Smiley mode
   $scope.loading = true;
   SmileyService.get($stateParams.smileyId)
-    .$loaded()
-    .then(function(data) {
-      $scope.smiley = data;
-      $scope.loading = false;
+  .$loaded()
+  .then(function(data) {
+    $scope.smiley = data;
+    $scope.loading = false;
 
 
-      // Find users other posts
-      if (data.nick) {
-        var usersSmileysQuery = SmileyService.findByUser(data.nick);
+    // Find users other posts
+    if (data.nick) {
+      var usersSmileysQuery = SmileyService.findByUser(data.nick);
 
-        usersSmileysQuery.$loaded().then(function(data) {
-          $scope.smileysFromUser = data;
-        });
-      }
+      usersSmileysQuery.$loaded().then(function(data) {
+        $scope.smileysFromUser = data;
+      });
+    }
 
-    });
+  });
 
   $scope.getTimeAgo = Helpers.getTimeAgo;
 })
